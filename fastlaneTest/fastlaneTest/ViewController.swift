b3c6113aa0c470f27eaaa0e619621f29d95918b2
Linc <xiang.ling@zuolin.com>
2018-12-19 10:56:41 +0800

    project init

---
+++ fastlaneTest/fastlaneTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  fastlaneTest
+//
+//  Created by Linc on 2018/12/19.
+//  Copyright © 2018年 Linc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.setupUI()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func setupUI() {
+        
+    }
+}
+
